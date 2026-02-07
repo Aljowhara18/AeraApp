b62a30f7b034076c467785100ba6309ce4cf2fa5
Aljowhara18 <Aljowhara18@gmail.com>
2026-02-07 22:33:40 +0300

    الان

--- BurnexApp/BurnexAppApp.swift
+++ BurnexApp/BurnexAppApp.swift
@@ -19,7 +19,7 @@ struct BurnexAppApp: App {
         do {
             return try ModelContainer(for: schema, configurations: [modelConfiguration])
         } catch {
-            fatalError("Could not create ModelContainer: \(error)")
+            fatalError("Couldالالا not create ModelContainer: \(error)")
         }
     }()
 
