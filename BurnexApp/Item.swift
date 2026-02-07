c73ac29a8fcf3e9721d474aa192f42deb8bf8490
Aljowhara18 <Aljowhara18@gmail.com>
2026-02-07 22:26:45 +0300

    Initial Commit

---
+++ BurnexApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BurnexApp
+//
+//  Created by Jojo on 07/02/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
