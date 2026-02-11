80409dc3565a969124e031b5d15d90f09bd99a25
faitmh <faitmh.ab@gmail.com>
2026-02-11 13:05:17 +0300

    AddAppleWatch

---
+++ Burnexwatch Watch AppTests/Burnexwatch_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Burnexwatch_Watch_AppTests.swift
+//  Burnexwatch Watch AppTests
+//
+//  Created by Faitmh ibrahim on 23/08/1447 AH.
+//
+
+import Testing
+@testable import Burnexwatch_Watch_App
+
+struct Burnexwatch_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
